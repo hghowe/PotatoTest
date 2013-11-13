74c0ede4fedbdd6b5088dcae625b4bd261de388d
kadmin <kadmin@172.16.219.29>
2013-11-13 11:30:22 -0600

    First Commit

---
+++ src/potatotest/PotatoTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package potatotest;
+
+/**
+ *
+ * @author kadmin
+ */
+public class PotatoTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
