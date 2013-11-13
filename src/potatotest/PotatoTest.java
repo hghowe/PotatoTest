d3582e5547d2038cd263da7036421d28a5a1c9f8
kadmin <kadmin@172.16.219.29>
2013-11-13 12:15:49 -0600

    Created dummyMethod

--- src/potatotest/PotatoTest.java
+++ src/potatotest/PotatoTest.java
@@ -15,6 +15,11 @@ public class PotatoTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here    
+    }
+    
+    public void dummyMethod()
+    {
+        
     }
 }
